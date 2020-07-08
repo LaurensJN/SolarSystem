6f366e8e47ef8decaa53a6dbeeb0795b7fdb0e8c
Laurens Oostwegel <l.oostwegel@gmail.com>
2020-07-08 12:22:53 +0200

    Added planet metadata (orbital days; AU; Eccentricity)

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    public float sunOrbit;              // Days it takes for one orbit around sun
+    public float AU;                    // Relative distance semi-major axis compared to earth
+    public float eccentricity;          // Eccentricity of rotation around the sun
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/SolarSystemSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class SolarSystemSettings : MonoBehaviour
+{
+    [Range(0.0001f, 1)]
+    public double globalScale = 0.000001f;
+
+    [Range(0.0001f, 1f)]
+    public double distanceScale;
+
+    public bool sqrtDistance;
+
+    [Range(0.0001f, 500)]
+    public float framesPerDay = 30f;
+}
