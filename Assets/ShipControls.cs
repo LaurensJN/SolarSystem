ebd58a30555b4bc80345aa3f99ec36bc6797e47a
Laurens Oostwegel <l.oostwegel@gmail.com>
2020-08-07 14:46:35 +0200

    Added spaceship and camera follower

---
+++ Assets/ShipControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.rotation = transform.rotation;
+        mainCamera.transform.position = transform.position + transform.rotation * Vector3.back * 2;
+    }
+}
